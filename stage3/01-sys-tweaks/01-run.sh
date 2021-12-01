02494050e366d456cf66f88bb393f63729e1e782
GrantPerkins <gcperk20@gmail.com>
2021-11-30 20:07:28 -0500

    Update ML dependency to new namespace

--- stage3/01-sys-tweaks/01-run.sh
+++ stage3/01-sys-tweaks/01-run.sh
@@ -349,7 +349,7 @@ rm -rf "${EXTRACT_DIR}/pixy2/build"
 #
 
 on_chroot << EOF
-pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+apt-get install python3-tflite-runtime
 EOF
 
 #
